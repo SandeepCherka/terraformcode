50f165d0c1f96162927fff5161ab99eb99a1ce23
SandeepCherka <123619629+SandeepCherka@users.noreply.github.com>
2023-07-24 16:15:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -60,20 +60,3 @@ resource "aws_security_group" "four" {
   }
 }
 
-
-variable "instance_type" {
-
-}
-
-variable "instance_name_1" {
-
-}
-
-variable "instance_name_2" {
-
-}
-
-variable "instance_name_3" {
-
-}
-
